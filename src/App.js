a04c844abea6246334035c14939bccbe639928b5
Mircea Dumitrescu <mircea.dumitrescu@3pillarglobal.com>
2022-10-03 08:52:03 +0200

    fixes

--- src/App.js
+++ src/App.js
@@ -16,9 +16,40 @@ const App = () => {
     setMovies(data.Search);
   };
 
+  // create a debounced search term
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
   useEffect(() => {
-    searchMovies("harry potter");
+
+    debouncedSearchTerm ? searchMovies(debouncedSearchTerm) : setMovies([]);
+
+  }, [debouncedSearchTerm]);
+
+  useEffect(() => {
+    searchMovies("avengers");
   }, []);
+
+  // Hook
+function useDebounce(value, delay) {
+  // State and setters for debounced value
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(
+    () => {
+      // Update debounced value after delay
+      const handler = setTimeout(() => {
+        setDebouncedValue(value);
+      }, delay);
+      // Cancel the timeout if value changes (also on delay change or unmount)
+      // This is how we prevent debounced value from updating if value is changed ...
+      // .. within the delay period. Timeout gets cleared and restarted.
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [value, delay] // Only re-call effect if value or delay changes
+  );
+  return debouncedValue;
+}
 
   return (
     <div className="app">
